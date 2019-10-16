02f619c53e862450e4266605465ad8401fdb4aad
1tejagowtham <1tejagowtham@gmail.com>
2019-10-16 13:51:00 +0530

    setting up our REACT APP

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "teja-notes"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://zx398uvymi.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: " us-east-2_7lhYVU2yC",
+    APP_CLIENT_ID: "3t5k9mshoefpnv7j6311mf3ekp",
+    IDENTITY_POOL_ID: "us-east-2:cc2140fd-c143-4523-8e2c-d0184cc7a6b2"
+  }
+};
